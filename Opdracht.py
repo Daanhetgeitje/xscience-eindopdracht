d42cd629138b99ada4f58502e868d8409f47b598
Daanhetgeitje <140931@edu.pcc.nu>
2024-11-20 09:06:36 +0000

    meer zinnen begin API

--- Opdracht.py
+++ Opdracht.py
@@ -10,11 +10,30 @@ patterns = {
 
 responses = {
   "Hallo": ["Hoi, hoe gaat het?", "Hallo!"],
-  "Het gaat goed": ["Goed om te horen!", "Geweldig!"]
-  "Wat kost een gemiddelde vlucht": ["De gemiddelde vlucht kost €354  "]
-  "wat kost een gemiddelde vlucht": ["De gemiddelde vlucht kost €354  "]
-  "Wanneer gaat de meesten vluchten":
+  "hallo": ["Hoi, hoe gaat het?", "Hallo!"],
+  "Het gaat goed": ["Goed om te horen!", "Geweldig!"],
+  "het gaat goed": ["Goed om te horen!", "Geweldig!"],
+  "Wat kost een gemiddelde vlucht": ["De gemiddelde vlucht kost €354  "],
+  "wat kost een gemiddelde vlucht": ["De gemiddelde vlucht kost €354  "],
+  "Wat kost een gemiddelde vlucht?": ["De gemiddelde vlucht kost €354  "],
+  "wat kost een gemiddelde vlucht?": ["De gemiddelde vlucht kost €354  "],
+  "Wanneer gaan de meesten vluchten": ["De meesten vluchten gaan tussen 15:00 en 20:00."],
+  "wanneer gaan de meesten vluchten": ["De meesten vluchten gaan tussen 15:00 en 20:00."],
+  "Wanneer gaan de meesten vluchten?": ["De meesten vluchten gaan tussen 15:00 en 20:00."],
+  "wanneer gaan de meesten vluchten?": ["De meesten vluchten gaan tussen 15:00 en 20:00."],
+  "Hoelang duurt het om een vlucht te boeken":["Een vlucht boeken duurt tussen de 2-3 minuten"],
+  "hoelang duurt het om een vlucht te boeken":["Een vlucht boeken duurt tussen de 2-3 minuten"],
+  "Hoelang duurt het om een vlucht te boeken?":["Een vlucht boeken duurt tussen de 2-3 minuten"],
+  "hoelang duurt het om een vlucht te boeken?":["Een vlucht boeken duurt tussen de 2-3 minuten"],
+  "Wat is het weer in Aruba?":[get_cat_fact()]
 }
+
+def get_cat_fact():
+  url = 'https://meowfacts.herokuapp.com/'
+  response = urllib.request.urlopen(url)
+  result = json.loads(response.read())
+
+  print(result)
 
 def get_response(message):
 
@@ -33,4 +52,4 @@ while True:
   message = input("YOU: ")
   response = get_response(message)
   print("Bot: " + response)- 
-  Ik ben Abel en ik ben gay
+  
